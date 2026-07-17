e95b769b5f311a403c3be1c045bae9486ce55fd6
Avishek Goswami <avishekgoswamiraj@gmail.com>
2026-07-17 18:59:15 +0530

    Time: 1 ms (97.59%), Space: 42.2 MB (95.8%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mySqrt(int x) 
+    {
+        long lo=0,hi=x;
+        while(lo<=hi){
+            long mid=lo+(hi-lo)/2;
+            if(mid*mid==x)
+            {
+                return (int)mid;
+                }
+            else if(mid*mid>x)
+            {
+                 hi=mid-1;
+                }
+            else
+            {
+                lo=mid+1;
+            }
+    }
+    return (int) hi;
+
+}
+}
